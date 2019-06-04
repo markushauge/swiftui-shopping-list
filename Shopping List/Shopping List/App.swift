300aaf9c7928cd9ce6fcdae336c5426820404fd5
fshauge <markus.hauge@hotmail.com>
2019-06-05 00:36:49 +0200

    Add simple list containing each product

--- Shopping List/Shopping List/App.swift
+++ Shopping List/Shopping List/App.swift
@@ -1,8 +1,39 @@
 import SwiftUI
 
-struct App : View {
+struct Product {
+    var name: String
+}
+
+struct ProductRow : View {
+    @State var product: Product
+    
     var body: some View {
-        Text("Hello World")
+        Text(product.name)
+    }
+}
+
+struct ProductList : View {
+    @State var products: [Product]
+    
+    var body: some View {
+        List(products.identified(by: \.name)) { product in
+            ProductRow(product: product)
+        }
+    }
+}
+
+struct App : View {
+    @State var products: [Product] = [
+        .init(name: "Milk"),
+        .init(name: "Butter"),
+        .init(name: "Eggs")
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ProductList(products: products)
+                .navigationBarTitle(Text("Shopping List"))
+        }
     }
 }
 
